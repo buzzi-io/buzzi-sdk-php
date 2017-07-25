9a86c40f24f13819cf1918f211ad9fca2b8f991b
Yurii Torbyk <jora.yt@gmail.com>
2017-07-25 12:20:16 +0300

    Refactoring, add new endpoints

--- src/Buzzi/Consume/Delivery.php
+++ src/Buzzi/Consume/Delivery.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Buzzi;
+namespace Buzzi\Consume;
 
 use Buzzi\Utils\StringUtils;
 
--- src/Buzzi/Events/Generic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Buzzi\Events;
-
-class Generic
-{
-    const FILES_UPLOAD = 'buzzi.generic.files-upload';
-    const TEST         = 'buzzi.generic.test';
-}
